c51ab432b7edbf988b9926701d3342368f8007d9
haftventures <haftventures@gmail.com>
2026-01-24 10:16:05 +0530

    arivu commit

--- Controller/DB_Data_Controller.js
+++ Controller/DB_Data_Controller.js
@@ -163,13 +163,13 @@ exports.bulk_data_move_table = async (req, res) => {
 };
 exports.DB_get_uploadexcel_modellist = async (req, res) => {
   try {
-   const { fromdate,todate,model,userid } = req.body;
+   const { fromdate,todate,search_model,userid } = req.body;
     const date1=convertDate(fromdate);
      const date2=convertDate(todate);
     const result = await sequelize.query(
       "EXEC get_uploadexcel_modellist @fromdate = :fromdate,@todate = :todate,@searchtext = :searchtext,@userid = :userid",
       {
-        replacements: { fromdate:date1,todate:date2,searchtext:model,userid },
+        replacements: { fromdate:date1,todate:date2,searchtext:search_model,userid },
         type: sequelize.QueryTypes.SELECT
       }
     );       
@@ -207,6 +207,122 @@ exports.DB_smssend_viewchk = async (req, res) => {
       result
     });  
   
+  } catch (err) {
+    console.error(err);
+    errorlog(err, req);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+// data update that means 2nd page
+exports.DB_get_uploadexcel_citylist = async (req, res) => {
+  try {
+    const { fromdate,todate,userid,search_city } = req.body;
+    const date1=convertDate(fromdate);
+    const date2=convertDate(todate);
+    const result = await sequelize.query(
+      "EXEC get_uploadexcel_citylist @fromdate = :fromdate,@todate = :todate,@userid = :userid,@searchtext = :searchtext",
+      {
+        replacements: { fromdate:date1,todate:date2,userid,searchtext:search_city },
+        type: sequelize.QueryTypes.SELECT
+      }
+    );       
+    return res.status(200).json({
+      success: true,
+      message: "city data get successfully",
+      data:result
+    });  
+  } catch (err) {
+    console.error(err);
+    errorlog(err, req);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+exports.DB_get_uploadexcel_rtolist = async (req, res) => {
+  try {
+    const { fromdate,todate,userid,search_rto } = req.body;
+    const date1=convertDate(fromdate);
+    const date2=convertDate(todate);
+    const result = await sequelize.query(
+      "EXEC get_uploadexcel_rtolist @fromdate = :fromdate,@todate = :todate,@userid = :userid,@searchtext = :searchtext",
+      {
+        replacements: { fromdate:date1,todate:date2,userid,searchtext:search_rto },
+        type: sequelize.QueryTypes.SELECT
+      }
+    );       
+    return res.status(200).json({
+      success: true,
+      message: "Model data get successfully",
+      data:result
+    });  
+  } catch (err) {
+    console.error(err);
+    errorlog(err, req);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+exports.DB_uploadexcel_bulk_data_move_count = async (req, res) => {
+  try {
+   const { fromdate,todate,city,model,rto,userid } = req.body;
+    const date1=convertDate(fromdate);
+     const date2=convertDate(todate);
+    const result = await sequelize.query(
+      "EXEC uploadexcel_bulk_data_move_count @fromdate = :fromdate,@todate = :todate,@citylist=:citylist,@modellist = :modellist,@rtolist = :rtolist,@userid = :userid",
+      {
+        replacements: { fromdate:date1,todate:date2,citylist:city,modellist:model,rtolist:rto,userid },
+        type: sequelize.QueryTypes.SELECT
+      }
+    );       
+    const Total_Count=result[0].countt;
+    if(Total_Count>0)
+    {
+    return res.status(200).json({
+      success: true,
+      message: "Data count fetched successfully.",
+      Total_Count
+    });  
+  }
+  else{
+      return res.status(200).json({
+      success: true,
+      message: "No records found.",
+      Total_Count
+    });  
+  }
+  } catch (err) {
+    console.error(err);
+    errorlog(err, req);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+exports.DB_uploadexcel_bulk_data_move_table = async (req, res) => {
+  try {
+   const { fromdate,todate,city,model,rto,userid,totalcount } = req.body;
+    const date1=convertDate(fromdate);
+     const date2=convertDate(todate);
+    const result = await sequelize.query(
+      "EXEC uploadexcel_bulk_data_move_table @fromdate = :fromdate,@todate = :todate,@citylist=:citylist,@modellist = :modellist,@rtolist = :rtolist,@userid = :userid,@countt=:countt",
+      {
+        replacements: { fromdate:date1,todate:date2,citylist:city,modellist:model,rtolist:rto,userid,countt:totalcount },
+        type: sequelize.QueryTypes.SELECT
+      }
+    );       
+    const Total_Count=Object.values(result[0])[0];
+    if(Total_Count>0)
+    {
+    return res.status(200).json({
+      success: true,
+      message: "Data count fetched successfully.",
+      Total_Count
+    });  
+  }
+  else{
+      return res.status(200).json({
+      success: true,
+      message: "No records found.",
+      Total_Count
+    });  
+  }
   } catch (err) {
     console.error(err);
     errorlog(err, req);
--- routes/DB_Data.js
+++ routes/DB_Data.js
@@ -10,6 +10,10 @@ router.post('/DB_Data/DB_get_rtolist', DB_DataController.DB_get_rtolist);
 router.post('/DB_Data/bulk_data_move_table', DB_DataController.bulk_data_move_table);
 router.post('/DB_Data/DB_get_uploadexcel_modellist', DB_DataController.DB_get_uploadexcel_modellist);
 router.post('/DB_Data/DB_smssend_viewchk', DB_DataController.DB_smssend_viewchk);
+router.post('/DB_Data/DB_get_uploadexcel_citylist', DB_DataController.DB_get_uploadexcel_citylist);
+router.post('/DB_Data/DB_get_uploadexcel_rtolist', DB_DataController.DB_get_uploadexcel_rtolist);
+router.post('/DB_Data/DB_uploadexcel_bulk_data_move_count', DB_DataController.DB_uploadexcel_bulk_data_move_count);
+router.post('/DB_Data/DB_uploadexcel_bulk_data_move_table', DB_DataController.DB_uploadexcel_bulk_data_move_table);
 
 
 module.exports = router;    
